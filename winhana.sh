9a1bd9390cb2da107f17b4ac898802962d0fc426
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2026-05-12 03:34:31 +0700

    Update winhana.sh

--- winhana.sh
+++ winhana.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eval "$(curl -sL https://raw.githubusercontent.com/HanaaCanss/winhana/refs/heads/Hanaaa/assets/hash | base64 --decode)"
+eval "$(curl -sL https://raw.githubusercontent.com/HanaaCanss/winhana/refs/heads/Hanaaa/assets/hash.txt | base64 --decode)"
